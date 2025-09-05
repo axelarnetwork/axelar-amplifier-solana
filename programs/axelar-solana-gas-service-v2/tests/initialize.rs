68728b2665f0e9451e5f95bdde237d81d957366b
Nikola Ristić <ristic@risticnikola.com>
2025-09-05 10:52:55 +0200

    test: test add native gas

--- programs/axelar-solana-gas-service-v2/tests/initialize.rs
+++ programs/axelar-solana-gas-service-v2/tests/initialize.rs
@@ -2,7 +2,7 @@
 
 use anchor_lang::Key;
 use axelar_solana_gas_service_v2::state::Treasury;
-use mollusk_svm::program::keyed_account_for_system_program;
+use mollusk_svm::{program::keyed_account_for_system_program, result::Check};
 use {
     anchor_lang::{
         solana_program::instruction::Instruction, system_program, Discriminator, InstructionData,
@@ -182,4 +182,100 @@ fn test_initialize_unauthorized() {
         operator_pda,
         &operator_pda_account,
     );
+}
+
+#[test]
+fn test_add_native_gas() {
+    // Setup
+
+    let program_id = axelar_solana_gas_service_v2::id();
+    let mut mollusk = setup_mollusk(&program_id, "axelar_solana_gas_service_v2");
+
+    let operator = Pubkey::new_unique();
+    let operator_account = Account::new(1_000_000_000, 0, &system_program::ID);
+
+    let (operator_pda, operator_pda_account) =
+        setup_operator(&mut mollusk, operator, &operator_account);
+
+    let (treasury, treasury_pda) = init_gas_service(
+        &mollusk,
+        operator,
+        &operator_account,
+        operator_pda,
+        &operator_pda_account,
+    );
+
+    // Instruction
+
+    let sender = Pubkey::new_unique();
+    let sender_balance = 1_000_000_000u64; // 1 SOL
+    let sender_account = Account::new(sender_balance, 0, &system_program::ID);
+
+    let tx_hash = [0u8; 64];
+    let log_index = 0u64;
+    let gas_fee_amount = 300_000_000u64; // 0.3 SOL
+    let refund_address = Pubkey::new_unique();
+
+    let (event_authority, _bump) =
+        Pubkey::find_program_address(&[b"__event_authority"], &program_id);
+    let event_authority_account = Account::new(0, 0, &system_program::ID);
+
+    let ix = Instruction {
+        program_id,
+        accounts: axelar_solana_gas_service_v2::accounts::AddNativeGas {
+            sender,
+            treasury,
+            system_program: system_program::ID,
+            // Event authority
+            event_authority: event_authority,
+            // The current program account
+            program: program_id,
+        }
+        .to_account_metas(None),
+        data: axelar_solana_gas_service_v2::instruction::AddNativeGas {
+            tx_hash,
+            log_index,
+            gas_fee_amount,
+            refund_address,
+        }
+        .data(),
+    };
+
+    let accounts = vec![
+        (sender, sender_account.clone()),
+        (treasury, treasury_pda.clone()),
+        keyed_account_for_system_program(),
+        // Event authority
+        (event_authority, event_authority_account),
+        // Current program account (executable)
+        (
+            program_id,
+            Account {
+                lamports: 1,
+                data: vec![],
+                owner: bpf_loader_upgradeable::ID,
+                executable: true,
+                rent_epoch: 0,
+            },
+        ),
+    ];
+
+    // Checks
+
+    let checks = vec![
+        Check::success(),
+        // Balance subtracted
+        Check::account(&sender)
+            .lamports(sender_balance - gas_fee_amount)
+            .build(),
+        // Balance added
+        Check::account(&treasury)
+            .lamports(treasury_pda.lamports + gas_fee_amount)
+            .build(),
+    ];
+
+    let result = mollusk.process_and_validate_instruction(&ix, &accounts, &checks);
+    assert!(result.program_result.is_ok(), "should add native gas");
+
+    // TODO(v2) check for CPI event emission
 }
