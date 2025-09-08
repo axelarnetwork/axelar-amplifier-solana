ef9aed7f3263d95fb46770a908c3bd283e3f5c65
Nikola Ristić <ristic@risticnikola.com>
2025-09-08 13:35:11 +0200

    test: add native gas related tests

--- programs/axelar-solana-gas-service-v2/tests/initialize.rs
+++ programs/axelar-solana-gas-service-v2/tests/initialize.rs
@@ -2,6 +2,7 @@
 
 use anchor_lang::Key;
 use axelar_solana_gas_service_v2::state::Treasury;
+use axelar_solana_operators::{OperatorAccount, OperatorRegistry};
 use mollusk_svm::{program::keyed_account_for_system_program, result::Check};
 use {
     anchor_lang::{
@@ -15,12 +16,12 @@ use {
 
 // TODO(v2) extract to a common test utils crate
 // or set the env var differently
-pub fn setup_mollusk(program_id: &Pubkey, program_name: &str) -> Mollusk {
+pub(crate) fn setup_mollusk(program_id: &Pubkey, program_name: &str) -> Mollusk {
     std::env::set_var("SBF_OUT_DIR", "../../target/deploy");
     Mollusk::new(program_id, program_name)
 }
 
-pub fn setup_operator(
+pub(crate) fn setup_operator(
     mollusk: &mut Mollusk,
     operator: Pubkey,
     operator_account: &Account,
@@ -61,6 +62,14 @@ pub fn setup_operator(
         data: axelar_solana_operators::instruction::Initialize {}.data(),
     };
 
+    let checks1 = vec![
+        Check::success(),
+        Check::account(&registry)
+            .space(OperatorRegistry::DISCRIMINATOR.len() + OperatorRegistry::INIT_SPACE)
+            .build(),
+        Check::all_rent_exempt(),
+    ];
+
     // Add operator instruction
     let ix2 = Instruction {
         program_id,
@@ -75,6 +84,14 @@ pub fn setup_operator(
         data: axelar_solana_operators::instruction::AddOperator {}.data(),
     };
 
+    let checks2 = vec![
+        Check::success(),
+        Check::account(&operator_pda)
+            .space(OperatorAccount::DISCRIMINATOR.len() + OperatorAccount::INIT_SPACE)
+            .build(),
+        Check::all_rent_exempt(),
+    ];
+
     // List accounts
     let accounts = vec![
         (operator, operator_account.clone()),
@@ -83,8 +100,15 @@ pub fn setup_operator(
         keyed_account_for_system_program(),
     ];
 
-    let result = mollusk.process_instruction_chain(&[ix1, ix2], &accounts);
-    assert!(result.program_result.is_ok());
+    let result = mollusk.process_and_validate_instruction_chain(
+        &[
+            // Initialize the registry
+            (&ix1, &checks1),
+            // Add the operator
+            (&ix2, &checks2),
+        ],
+        &accounts,
+    );
 
     let operator_pda_account = result
         .get_account(&operator_pda)
@@ -93,7 +117,7 @@ pub fn setup_operator(
     (operator_pda, operator_pda_account.clone())
 }
 
-fn init_gas_service(
+pub(crate) fn init_gas_service(
     mollusk: &Mollusk,
     operator: Pubkey,
     operator_account: &Account,
@@ -125,15 +149,19 @@ fn init_gas_service(
         keyed_account_for_system_program(),
     ];
 
-    let result = mollusk.process_instruction(&ix, &accounts);
-    assert!(result.program_result.is_ok(), "should initialize");
+    let checks = vec![
+        Check::success(),
+        Check::account(&treasury)
+            .space(Treasury::DISCRIMINATOR.len() + Treasury::INIT_SPACE)
+            .build(),
+        Check::all_rent_exempt(),
+    ];
+
+    let result = mollusk.process_and_validate_instruction(&ix, &accounts, &checks);
 
     let treasury_pda = result
         .get_account(&treasury)
         .expect("Treasury PDA should exist");
-
-    let expected_size = Treasury::DISCRIMINATOR.len() + Treasury::INIT_SPACE;
-    assert_eq!(treasury_pda.data.len(), expected_size);
 
     (treasury, treasury_pda.clone())
 }
@@ -161,7 +189,7 @@ fn test_initialize_success() {
 }
 
 #[test]
-#[should_panic(expected = "should initialize")]
+#[should_panic]
 fn test_initialize_unauthorized() {
     let program_id = axelar_solana_gas_service_v2::id();
     let mut mollusk = setup_mollusk(&program_id, "axelar_solana_gas_service_v2");
@@ -182,100 +210,4 @@ fn test_initialize_unauthorized() {
         operator_pda,
         &operator_pda_account,
     );
-}
-
-#[test]
-fn test_add_native_gas() {
-    // Setup
-
-    let program_id = axelar_solana_gas_service_v2::id();
-    let mut mollusk = setup_mollusk(&program_id, "axelar_solana_gas_service_v2");
-
-    let operator = Pubkey::new_unique();
-    let operator_account = Account::new(1_000_000_000, 0, &system_program::ID);
-
-    let (operator_pda, operator_pda_account) =
-        setup_operator(&mut mollusk, operator, &operator_account);
-
-    let (treasury, treasury_pda) = init_gas_service(
-        &mollusk,
-        operator,
-        &operator_account,
-        operator_pda,
-        &operator_pda_account,
-    );
-
-    // Instruction
-
-    let sender = Pubkey::new_unique();
-    let sender_balance = 1_000_000_000u64; // 1 SOL
-    let sender_account = Account::new(sender_balance, 0, &system_program::ID);
-
-    let tx_hash = [0u8; 64];
-    let log_index = 0u64;
-    let gas_fee_amount = 300_000_000u64; // 0.3 SOL
-    let refund_address = Pubkey::new_unique();
-
-    let (event_authority, _bump) =
-        Pubkey::find_program_address(&[b"__event_authority"], &program_id);
-    let event_authority_account = Account::new(0, 0, &system_program::ID);
-
-    let ix = Instruction {
-        program_id,
-        accounts: axelar_solana_gas_service_v2::accounts::AddNativeGas {
-            sender,
-            treasury,
-            system_program: system_program::ID,
-            // Event authority
-            event_authority: event_authority,
-            // The current program account
-            program: program_id,
-        }
-        .to_account_metas(None),
-        data: axelar_solana_gas_service_v2::instruction::AddNativeGas {
-            tx_hash,
-            log_index,
-            gas_fee_amount,
-            refund_address,
-        }
-        .data(),
-    };
-
-    let accounts = vec![
-        (sender, sender_account.clone()),
-        (treasury, treasury_pda.clone()),
-        keyed_account_for_system_program(),
-        // Event authority
-        (event_authority, event_authority_account),
-        // Current program account (executable)
-        (
-            program_id,
-            Account {
-                lamports: 1,
-                data: vec![],
-                owner: bpf_loader_upgradeable::ID,
-                executable: true,
-                rent_epoch: 0,
-            },
-        ),
-    ];
-
-    // Checks
-
-    let checks = vec![
-        Check::success(),
-        // Balance subtracted
-        Check::account(&sender)
-            .lamports(sender_balance - gas_fee_amount)
-            .build(),
-        // Balance added
-        Check::account(&treasury)
-            .lamports(treasury_pda.lamports + gas_fee_amount)
-            .build(),
-    ];
-
-    let result = mollusk.process_and_validate_instruction(&ix, &accounts, &checks);
-    assert!(result.program_result.is_ok(), "should add native gas");
-
-    // TODO(v2) check for CPI event emission
 }
