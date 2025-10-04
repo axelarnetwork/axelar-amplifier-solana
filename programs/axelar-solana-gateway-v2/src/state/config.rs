817f09f6a1e0b6aa8839dc976cb15b4e13269e67
MakisChristou <michalis.christou@protonmail.com>
2025-10-04 14:58:57 +0300

    fix: separate verifier set tracker

--- programs/axelar-solana-gateway-v2/src/state/config.rs
+++ programs/axelar-solana-gateway-v2/src/state/config.rs
@@ -62,17 +62,3 @@ pub struct InitializeConfigParams {
     /// how many n epochs do we consider valid
     pub previous_verifier_retention: VerifierSetEpoch,
 }
-
-/// Ever-incrementing counter for keeping track of the sequence of signer sets
-pub type Epoch = U256;
-
-#[account]
-#[derive(Debug, PartialEq, Eq)]
-pub struct VerifierSetTracker {
-    /// The canonical bump for this account.
-    pub bump: u8,
-    /// The epoch associated with this verifier set
-    pub epoch: Epoch,
-    /// The verifier set hash
-    pub verifier_set_hash: VerifierSetHash,
-}
