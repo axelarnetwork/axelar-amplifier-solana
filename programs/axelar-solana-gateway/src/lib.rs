78515469d346b9f37f27ea4c18db38233fb098f1
isaac markus <isaac@interoplabs.io>
2025-10-06 14:19:49 -0400

    fix: update gateway pid

--- programs/axelar-solana-gateway/src/lib.rs
+++ programs/axelar-solana-gateway/src/lib.rs
@@ -22,7 +22,7 @@ use solana_program::pubkey::{Pubkey, PubkeyError};
 ensure_single_feature!("devnet-amplifier", "stagenet", "testnet", "mainnet");
 
 #[cfg(feature = "devnet-amplifier")]
-solana_program::declare_id!("gtwi5T9x6rTWPtuuz6DA7ia1VmH8bdazm9QfDdi6DVp");
+solana_program::declare_id!("3jSHFCZkd3B751yP1qK2MYF8rvGFgaAACzb2GKsxM7m7");
 
 #[cfg(feature = "stagenet")]
 solana_program::declare_id!("gtwqQzBirGUVdAUDt17WWYnVfPoydn9eAazDJb7gFUs");
