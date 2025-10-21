a5c756b9d3d50ae9deeab1667aaccf310d859270
Nikola Ristić <ristic@risticnikola.com>
2025-10-21 23:55:00 +0200

    fix: use session signing_verifier_set_hash as set for the session
    - instead of verifier_set_tracker to match v1

--- programs/axelar-solana-gateway-v2/src/instructions/verify_signature.rs
+++ programs/axelar-solana-gateway-v2/src/instructions/verify_signature.rs
@@ -13,10 +13,10 @@ pub struct VerifySignature<'info> {
         bump = gateway_root_pda.load()?.bump,
         // Check: Verifier domain separator matches the gateway's domain separator
         constraint = gateway_root_pda.load()?.domain_separator == verifier_info.leaf.domain_separator
-        	@ GatewayError::InvalidDomainSeparator,
+            @ GatewayError::InvalidDomainSeparator,
         // Check: Verifier set isn't expired
         constraint = gateway_root_pda.load()?.assert_valid_epoch(verifier_set_tracker_pda.load()?.epoch).is_ok()
-        	@ GatewayError::VerifierSetTooOld,
+            @ GatewayError::VerifierSetTooOld,
     )]
     pub gateway_root_pda: AccountLoader<'info, GatewayConfig>,
 
@@ -25,17 +25,20 @@ pub struct VerifySignature<'info> {
         seeds = [
             SignatureVerificationSessionData::SEED_PREFIX,
             merkle_root.as_ref(),
-            verifier_set_tracker_pda.load()?.verifier_set_hash.as_ref(),
+            verification_session_account.load()?.signature_verification.signing_verifier_set_hash.as_ref(),
         ],
         bump = verification_session_account.load()?.bump
     )]
     pub verification_session_account: AccountLoader<'info, SignatureVerificationSessionData>,
 
     #[account(
-		// The verifier set tracker PDA is derived from the verifier set hash
-		seeds = [VerifierSetTracker::SEED_PREFIX, verifier_set_tracker_pda.load()?.verifier_set_hash.as_ref()],
-		bump,
-	)]
+        // The verifier set tracker PDA is derived from the verifier set hash
+        seeds = [
+            VerifierSetTracker::SEED_PREFIX,
+            verifier_set_tracker_pda.load()?.verifier_set_hash.as_ref(),
+        ],
+        bump,
+    )]
     pub verifier_set_tracker_pda: AccountLoader<'info, VerifierSetTracker>,
 }
 
