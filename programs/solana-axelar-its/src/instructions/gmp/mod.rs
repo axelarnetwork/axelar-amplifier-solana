b2029bbf94068e56e45e9ce951f4c03bb3bf58d4
Nikola Ristić <ristic@risticnikola.com>
2025-11-05 16:28:28 +0100

    fix: don't reexport internal modules

--- programs/solana-axelar-its/src/instructions/gmp/mod.rs
+++ programs/solana-axelar-its/src/instructions/gmp/mod.rs
@@ -1,11 +1,11 @@
-pub mod deploy_interchain_token;
+mod deploy_interchain_token;
 pub use deploy_interchain_token::*;
 
 pub mod execute;
 pub use execute::*;
 
-pub mod link_token;
+mod link_token;
 pub use link_token::*;
 
-pub mod interchain_transfer;
+mod interchain_transfer;
 pub use interchain_transfer::*;
