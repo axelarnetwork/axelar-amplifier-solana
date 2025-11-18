5807527c0841e4f6e0776abd1887b9a792ab5480
Makis Christou <Makis_Christou@protonmail.com>
2025-11-18 21:13:45 +0200

    fix: allow token manager operator to add himself as flow limiter (#95)
    Fix Arda's finding 157.

--- programs/solana-axelar-its/src/instructions/token_manager/remove_token_manager_flow_limiter.rs
+++ programs/solana-axelar-its/src/instructions/token_manager/remove_token_manager_flow_limiter.rs
@@ -1,7 +1,4 @@
-use crate::{
-    state::{InterchainTokenService, Roles, RolesError, TokenManager, UserRoles},
-    ItsError,
-};
+use crate::state::{InterchainTokenService, Roles, RolesError, TokenManager, UserRoles};
 use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
@@ -46,9 +43,6 @@ pub struct RemoveTokenManagerFlowLimiter<'info> {
     pub token_manager_pda: Account<'info, TokenManager>,
 
     /// The user account from which the FLOW_LIMITER role will be removed
-    #[account(
-        constraint = target_user_account.key() != authority_user_account.key() @ ItsError::InvalidArgument,
-    )]
     pub target_user_account: AccountInfo<'info>,
 
     /// Target user roles account (must exist and have FLOW_LIMITER role)
