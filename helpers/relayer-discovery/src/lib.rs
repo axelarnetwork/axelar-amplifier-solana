65548210102e19fb8e8f989ff1cc47d4b39bd440
Foivos <foivos@umich.edu>
2025-12-09 15:57:36 +0200

    remove relayer transaction accounts as they are not realistically very useful.

--- helpers/relayer-discovery/src/lib.rs
+++ helpers/relayer-discovery/src/lib.rs
@@ -11,8 +11,6 @@ use crate::structs::{RelayerAccount, RelayerData, RelayerInstruction, RelayerTra
 
 /// The structs for relayer discovery.
 pub mod structs;
-/// Helper transaction_pda utils.
-pub mod transaction_pda;
 
 /// The global transaction pda seed for all executables.
 pub const TRANSACTION_PDA_SEED: &[u8] = b"relayer-discovery-transaction";
--- programs/solana-axelar-test-discoverable/src/instructions/init.rs
+++ programs/solana-axelar-test-discoverable/src/instructions/init.rs
@@ -1,27 +1,36 @@
 use anchor_lang::prelude::*;
 use anchor_lang::Discriminator;
 use relayer_discovery::structs::{RelayerData, RelayerInstruction, RelayerTransaction};
-use relayer_discovery::transaction_pda_accounts;
 
 use crate::instruction::GetTransaction;
 
-transaction_pda_accounts!(relayer_transaction);
-
 #[derive(Accounts)]
 pub struct Init<'info> {
-    transaction: RelayerTransactionAccounts<'info>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    // IncomingMessage PDA account
+    // needs to be mutable as the validate_message CPI
+    // updates its state
+    #[account(
+        init,
+        seeds = [relayer_discovery::TRANSACTION_PDA_SEED],
+        bump,
+        payer = payer,
+        space = {
+            let mut bytes = Vec::with_capacity(256);
+            relayer_transaction().serialize(&mut bytes)?;
+            bytes.len()
+        }
+    )]
+    pub transaction: AccountInfo<'info>,
+
+    pub system_program: Program<'info, System>,
 }
 
 /// Initializes the relayer transaction for this executable.
 pub fn init_handler(ctx: Context<Init>) -> Result<()> {
-    relayer_transaction().serialize(
-        &mut &mut ctx
-            .accounts
-            .transaction
-            .relayer_transaction
-            .data
-            .borrow_mut()[..],
-    )?;
+    relayer_transaction().serialize(&mut &mut ctx.accounts.transaction.data.borrow_mut()[..])?;
 
     Ok(())
 }
