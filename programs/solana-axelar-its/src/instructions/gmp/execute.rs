c49713c74b74dde091af94b2c4799f671c526dcf
Nikola Ristić <ristic@risticnikola.com>
2025-12-16 23:24:04 +0100

    test: add draft test for destination=payer on gmp transfer

--- programs/solana-axelar-its/src/instructions/gmp/execute.rs
+++ programs/solana-axelar-its/src/instructions/gmp/execute.rs
@@ -390,7 +390,7 @@ pub fn execute_link_token_extra_accounts(
     let mut accounts = Vec::with_capacity(2);
 
     if let Some(key) = operator {
-        accounts.push(AccountMeta::new(key, false));
+        accounts.push(AccountMeta::new_readonly(key, false));
     }
 
     if let Some(pda_key) = operator_roles_pda {
