9be98ab704f02eeca5f90e94636f351cd685307b
Nikola Ristić <ristic@risticnikola.com>
2026-01-08 17:43:49 +0100

    chore: rm unused import

--- programs/solana-axelar-its/src/instructions/config/set_flow_limit.rs
+++ programs/solana-axelar-its/src/instructions/config/set_flow_limit.rs
@@ -1,6 +1,6 @@
 use crate::{
     events::FlowLimitSet,
-    state::{roles, InterchainTokenService, RolesError, TokenManager, UserRoles},
+    state::{InterchainTokenService, RolesError, TokenManager, UserRoles},
     ItsError,
 };
 use anchor_lang::prelude::*;
